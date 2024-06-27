ff9b88470a3c1ec5044f43c90e9ef082725d2f6b
Himay-Patel <himay.glsbca20@gmail.com>
2024-06-27 18:09:22 +0530

    initail commit

---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+
+export const InstraId = "https://www.instagram.com/patelhimay?igsh=MWtqOWxmd2Qyemk5NA=="
+
+export const GitHubId = "https://github.com/Himay-Patel"
+
+export const LinkedInId = "https://www.linkedin.com/in/himay-patel-512212312/"
+
+export const HERO_CONTENT = `I am an enthusiastic aspiring developer with a passion for creating robust and scalable web applications or android applications. Over the past year, I have been honing my skills in back-end technology like Node.js & Express, as well as gaining experience with basic front-end technologies such as ReactJS, NextJS & Jetpack Compose, with databases like MongoDB & MySQL`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile developer with a passion for creating efficient and user-friendly web applications & android applications.I have worked with a variety of technologies, including Jetpack Compose, React, Next.js, Node.js, Express, MongoDB database, MySQL database`;
+
+export const EXPERIENCES = [
+
+];
+
+export const PROJECTS = [
+    {
+        title: "Farmer's Assistant",
+        description:
+            "Developed an app that empowers new farmers with essential tools and up-to-date market prices for seamless crop selling. The app provides easy access to crop details, optimal market rates, fertilizer requirements, and supports animal trading through a user-friendly interface.",
+        technologies: ["MySQL", "PHP", "Android (Java, XML)"],
+        github_link: "https://github.com/Himay-Patel/FarmerAssistant.git",
+    },
+    {
+        title: "Chat App",
+        description:
+            "Developed a real-time messaging application enabling one-to-one communication. Implemented push notifications for new messages using Android Jetpack Compose with Kotlin and integrated Firebase for backend services.",
+        technologies: ["Firebase", "Jetpack Compose"],
+        github_link: "https://github.com/Himay-Patel/ComposeChatAppNew.git",
+    },
+    {
+        title: "Electronic-E-Hub (Currently Working)",
+        description:
+            "Developing a comprehensive online platform for an electronic store. The platform serves as a one-stop destination for customers to purchase a wide range of electronic items from the comfort of their homes.",
+        technologies: ["MongoDB", "Express", "NextJS", "Tailwind CSS", "Node.js"],
+        github_link: "https://github.com/Himay-Patel/electronics-e-hub.git",
+    },
+];
+
+export const CONTACT = {
+    address: "Ahmedabad, Gujarat, India",
+    phoneNo: "+91 8469664223 ",
+    email: "patelhimay2003@gmail.com",
+};
